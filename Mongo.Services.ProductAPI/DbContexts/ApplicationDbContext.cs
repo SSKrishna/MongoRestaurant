f8f7c1c250a28b824c861681bedd3d29ee63109b
Siddhartha Gunturu <gunturu.siddhartha@gmail.com>
2023-01-16 16:36:23 -0500

    Configures application to use connection string for MongoProductAPI

---
+++ Mongo.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mongo.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+    }
+}
