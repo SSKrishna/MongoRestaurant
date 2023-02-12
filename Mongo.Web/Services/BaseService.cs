17b6e067090cd0107d5f586a44f22426cd7a9dec
Siddhartha Gunturu <gunturu.siddhartha@gmail.com>
2023-02-12 13:28:42 -0500

    BAse Service Interface and API Request Model

---
+++ Mongo.Web/Services/BaseService.cs
@@ -0,0 +1,6 @@
+﻿namespace Mongo.Web.Services
+{
+    public class BaseService
+    {
+    }
+}
---
+++ Mongo.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Mongo.Web.Models;
+
+namespace Mongo.Web.Services.IServices
+{
+    public interface IBaseService:IDisposable
+    {
+        ResponseDto responseModel { get; set; }
+
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
