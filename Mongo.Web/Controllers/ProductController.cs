d27ab257abb14d7c5fb042857c1f2f3772be89ba
Siddhartha Gunturu <gunturu.siddhartha@gmail.com>
2023-02-20 21:34:59 -0500

    Product Index, Edit and Delete

--- Mongo.Web/Controllers/ProductController.cs
+++ Mongo.Web/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Mongo.Web.Models;
 using Mongo.Web.Services.IServices;
 using Newtonsoft.Json;
+using System.Data;
 
 namespace Mongo.Web.Controllers
 {
@@ -26,5 +29,90 @@ namespace Mongo.Web.Controllers
 
             return View(list);
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> ProductCreate()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDto model)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync<ResponseDto>(model);
+                if(response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }           
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            ResponseDto response = await _productService.GetProductsByIdAsync<ResponseDto>(productId);
+            if (response != null && response.IsSuccess)
+            {
+                 var product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(product);
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductEdit(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductDelete(int productId)
+        {
+            ResponseDto response = await _productService.GetProductsByIdAsync<ResponseDto>(productId);
+            if (response != null && response.IsSuccess)
+            {
+                ProductDto product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(product);
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductDelete(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.DeleteProductAsync<ResponseDto>(model.ProductId);
+                if (response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+        }
+
+
     }
 }
