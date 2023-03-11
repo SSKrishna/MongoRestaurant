43c65c4505711839ece10f6ba65de1dc846534d0
Siddhartha Gunturu <gunturu.siddhartha@gmail.com>
2023-03-11 15:47:08 -0500

    Updated Changes

--- Mango.Services.Identity/Program.cs
+++ Mango.Services.Identity/Program.cs
@@ -1,6 +1,34 @@
+using Mango.Services.Identity;
+using Mango.Services.Identity.DbContexts;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                                                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+var build = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents= true;
+    options.Events.RaiseInformationEvents= true;
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseSuccessEvents= true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(SD.IdentityResources)
+.AddInMemoryApiScopes(SD.ApiScopes)
+.AddInMemoryClients(SD.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+build.AddDeveloperSigningCredential();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -17,7 +45,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
