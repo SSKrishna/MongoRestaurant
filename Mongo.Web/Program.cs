70024986a05fa056bffff77c5013ed5f2fbaa4dd
Siddhartha Gunturu <gunturu.siddhartha@gmail.com>
2023-03-21 00:21:12 -0400

    Setup main project to use Identity Server

--- Mongo.Web/Program.cs
+++ Mongo.Web/Program.cs
@@ -6,6 +6,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(options =>
+                        {
+                            options.DefaultScheme = "Cookies";
+                            options.DefaultChallengeScheme = "oidc";
+                        })
+    .AddCookie("Cookies",c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+    .AddOpenIdConnect("oidc",options =>
+    {
+        options.Authority = builder.Configuration["ServiceUrls:IdentityAPI"];
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClientId = "mango";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType= "role";
+
+        options.Scope.Add("mango");
+
+        options.SaveTokens = true;
+    });
+
+
 builder.Services.AddRazorPages()
     .AddRazorRuntimeCompilation();
 
@@ -29,7 +52,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
